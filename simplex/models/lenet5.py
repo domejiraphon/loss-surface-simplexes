adacf191c3c3ec803f0e5421c4e703a1557f5736
mittalgovind <mittal@nyu.edu>
2021-12-03 17:29:48 -0500

    add lenet5 simplex

--- simplex/models/lenet5.py
+++ simplex/models/lenet5.py
@@ -20,22 +20,23 @@ from simplex_models import Conv2d as SimpConv
 class Lenet5Simplex(nn.Module):
     def __init__(self, num_classes=10, fix_points=[True]):
         super().__init__()
-        self.conv1 = SimpConv(in_channels=1, out_channels=6, padding=2,
+        self.conv1 = SimpConv(in_channels=1, out_channels=6,
                               stride=1, kernel_size=5, fix_points=fix_points)
         self.conv2 = SimpConv(in_channels=6, out_channels=16,
                               stride=1, kernel_size=5, fix_points=fix_points)
-        self.fc1 = SimpLinear(256, 120, fix_points=fix_points)
+        self.conv3 = SimpConv(in_channels=16, out_channels=120, kernel_size=5,
+                            fix_points=fix_points)
         self.fc2 = SimpLinear(120, 84, fix_points=fix_points)
         self.fc3 = SimpLinear(84, num_classes, fix_points=fix_points)
 
         self.relu = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d(2)
 
-    def forward(self, x, coeffs_t):
+    def forward(self, x, coeffs_t=[1]):
         x = self.pool(self.relu(self.conv1(x, coeffs_t)))
-        x = self.pool(self.relu(self.conv1(x, coeffs_t)))
+        x = self.pool(self.relu(self.conv2(x, coeffs_t)))
+        x = self.relu(self.conv3(x, coeffs_t))
         x = x.view(x.shape[0], -1)
-        x = self.fc1(x, coeffs_t)
         x = self.fc2(x, coeffs_t)
         x = self.fc3(x, coeffs_t)
         return x
